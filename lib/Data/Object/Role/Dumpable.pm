69765993f2610375e1fc98e9d847276f506e51b3
Adam Hopkins <violapiratejunky@gmail.com>
2019-09-18 18:23:06 -0400

    Adding pretty_dump, pretty_print, and pretty_say to Data::Object::Role::Dumpable (#294)

--- lib/Data/Object/Role/Dumpable.pm
+++ lib/Data/Object/Role/Dumpable.pm
@@ -47,4 +47,53 @@ sub say {
   return CORE::print(map +(&dump($_), "\n"), @args, $self);
 }
 
+sub pretty_dump {
+  my ($data) = @_;
+
+  require Data::Dumper;
+  require Data::Object::Utility;
+
+  no warnings 'once';
+
+  local $Data::Dumper::Indent = 2;
+  local $Data::Dumper::Trailingcomma = 1;
+  local $Data::Dumper::Purity = 0;
+  local $Data::Dumper::Pad = '';
+  local $Data::Dumper::Varname = 'VAR';
+  local $Data::Dumper::Useqq = 0;
+  local $Data::Dumper::Terse = 1;
+  local $Data::Dumper::Freezer = '';
+  local $Data::Dumper::Toaster = '';
+  local $Data::Dumper::Deepcopy = 1;
+  local $Data::Dumper::Quotekeys = 0;
+  local $Data::Dumper::Bless = 'bless';
+  local $Data::Dumper::Pair = ' => ';
+  local $Data::Dumper::Maxdepth = 0;
+  local $Data::Dumper::Maxrecurse = 1000;
+  local $Data::Dumper::Useperl = 0;
+  local $Data::Dumper::Sortkeys = 1;
+  local $Data::Dumper::Deparse = 1;
+  local $Data::Dumper::Sparseseen = 0;
+
+  $data = Data::Object::Utility::DetractDeep($_[0]);
+  $data = Data::Dumper::Dumper($data);
+  $data =~ s/^'|'$//g;
+
+  chomp $data;
+
+  return $data;
+}
+
+sub pretty_print {
+  my ($self, @args) = @_;
+
+  return CORE::print(map &pretty_dump($_), @args, $self);
+}
+
+sub pretty_say {
+  my ($self, @args) = @_;
+
+  return CORE::print(map +(&pretty_dump($_), "\n"), @args, $self);
+}
+
 1;
